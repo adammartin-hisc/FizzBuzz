73fdfe02e28dd44d6ed93a6b91d79d995d53138e
Adam Martin <adam.martin@homeinsteadinc.com>
2015-04-02 13:16:57 -0500

    Real Tests have started to form

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -8,5 +8,11 @@ namespace FizzBuzz
 {
     public class FizzBuzz
     {
+        public string Calculate(int p)
+        {
+            if (p % 5 == 0) return "Buzz";
+            if (p % 3 == 0) return "Fizz";
+            return p.ToString();
+        }
     }
 }
--- FizzBuzzTest/FizzBuzzTest.cs
+++ FizzBuzzTest/FizzBuzzTest.cs
@@ -1,14 +1,40 @@
 ﻿using System;
 using Xunit;
+using FluentAssertions;
 
 namespace FizzBuzzTest
 {
     public class FizzBuzzTest
     {
-        [Fact]
-        public void CanCreateFizzBuzzClass()
-        {
+        private FizzBuzz.FizzBuzz _fizzBuzz;
 
+        public FizzBuzzTest()
+        {
+            _fizzBuzz = new FizzBuzz.FizzBuzz();
+        }
+
+        [Fact]
+        public void OneIsOne()
+        {
+            _fizzBuzz.Calculate(1).Should().Be("1");
+        }
+
+        [Fact]
+        public void TwoIsTwo()
+        {
+            _fizzBuzz.Calculate(2).Should().Be("2");
+        }
+
+        [Fact]
+        public void ThreeIsFizzBecauseItIsDisableByThree()
+        {
+            _fizzBuzz.Calculate(3).Should().Be("Fizz");
+        }
+
+        [Fact]
+        public void FiveIsBuzzBecauseItIsDivisibleByFive()
+        {
+            _fizzBuzz.Calculate(5).Should().Be("Buzz");
         }
     }
 }
